24d0686e499fddb693ed8cce835f5fa89835c6ad
Domiciano <guilhermedomicianosilva@gmail.com>
2024-09-14 09:11:20 -0300

    fix botao perfil

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,6 @@
+// Marcar como Client Component
+"use client";
+
 export default function Page() {
     return (
         <div>
@@ -18,25 +21,26 @@ export default function Page() {
                     <button>Chat</button>
                 </li>
                 <li>
+                    {/* Correção no onClick */}
                     <button onClick={() => window.location.href = 'http://localhost:3000/perfil'}>Perfil</button>
                 </li>
             </ul>
             <div>
-                <input type="text" placeholder="O que quero aprener hoje"></input>
-                <h1>Olá Inserir nome</h1>
-                <div>
+                <input type="text" placeholder="O que quero aprender hoje" />
+                <h1>Olá, Inserir nome</h1>
+                <div style={{ display: 'flex', gap: '20px' }}>
                     <div>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s"></img>
-                        <p><strong>Calculo II</strong></p>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s" alt="Calculo II" />
+                        <p><strong>Cálculo II</strong></p>
                         <p>6 Monitores</p>
                     </div>
                     <div>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s"></img>
-                        <p><strong>Bioquimica Lab.</strong></p>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s" alt="Bioquimica Lab" />
+                        <p><strong>Bioquímica Lab.</strong></p>
                         <p>27 Monitores</p>
                     </div>
                     <div>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s"></img>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7vnGsgBgV8QW50dp-wZ4GoCNWu4egKYuxAw&s" alt="Fund. IA" />
                         <p><strong>Fund. IA</strong></p>
                         <p>10 Monitores</p>
                     </div>
