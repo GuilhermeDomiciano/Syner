88ed004d89c3198ee62042060525aaa9e62b31c2
nick <nicolefranca@rede.ulbra.br>
2024-09-14 09:48:06 -0300

    add button calculo

--- src/app/calculo/page.tsx
+++ src/app/calculo/page.tsx
@@ -1,51 +1,64 @@
-
+import Calculo from './img/Imagem.png';
+import Image from 'next/image';
 export default function Page() {
     return (
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Document</title>
-        </head>
-        <body>
-        <img src="C:\Users\nicolefranca\Downloads\image 8Imagem.png" alt="foto" />
-        <h1>Calculo II</h1>
-        <h3>6 monitores</h3>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        
         <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Silas Oliveira</h3>
-            <p>Monitor de Matemática</p>
+            <ul>
+                <li>
+                    <button>Início</button>
+                </li>
+                <li>
+                    <button>Comunidade</button>
+                </li>
+                <li>
+                    <button>Recados</button>
+                </li>
+                <li>
+                    <button>Agenda</button>
+                </li>
+                <li>
+                    <button>Chat</button>
+                </li>
+                <li>
+                    <button>Perfil</button>
+                </li>
+            </ul>
+            <Image src={Calculo} alt="foto" />
+            <h1>Calculo II</h1>
+            <h3>6 monitores</h3>
+            <br></br>
+
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Silas Oliveira</h3>
+                <p>Monitor de Matemática</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Amon Silva</h3>
+                <p>Monitor de Matemática</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Cleide Nascimento</h3>
+                <p>Monitor de Matemática</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Josefina Silva</h3>
+                <p>Monitor de Matemática</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Maria Paula</h3>
+                <p>Monitor de Matemática</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
+                <h3>Simone Lucas</h3>
+                <p>Monitor de Matemática</p>
+            </div>
         </div>
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Amon Silva</h3>
-            <p>Monitor de Matemática</p>
-        </div>
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Cleide Nascimento</h3>
-            <p>Monitor de Matemática</p>
-        </div>
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Josefina Silva</h3>
-            <p>Monitor de Matemática</p>
-        </div>
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Maria Paula</h3>
-            <p>Monitor de Matemática</p>
-        </div>
-        <div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7JRe80D-AqarDoS7AP-1r6unr_JdefroaKw&s" alt="user" />
-            <h3>Simone Lucas</h3>
-            <p>Monitor de Matemática</p>
-        </div>
-        </body>
-        </html>
     )
 }
