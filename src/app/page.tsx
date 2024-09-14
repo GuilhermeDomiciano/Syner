51541134ff4b27fd66cd3b2cc91e23399d870fb7
Domiciano <guilhermedomicianosilva@gmail.com>
2024-09-14 09:49:34 -0300

    testandinho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-    return <h1>Olá, Syner em Next.js!</h1>
+    return (
+        <div>
+            <h1>Olá, Syner em Next.js!</h1>
+            <p>apenas testano aqui</p>
+        </div>
+    );
 }
