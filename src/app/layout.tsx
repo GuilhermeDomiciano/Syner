8b3de1bc84fea4229153b2edad4e7a7892be6d26
DaviOGrande7 <davitx2005@gmail.com>
2024-11-11 19:39:03 -0300

    Cleaning layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-// src/app/layout.tsx
-
 import './globals.css';
-import ClientLayout from './components/ClientLayout'; // Importa o layout cliente
+import ClientLayout from './components/ClientLayout'; 
 
 export const metadata = {
   title: 'SYNER',
@@ -12,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <body>
-        <ClientLayout>{children}</ClientLayout> {/* Usa o layout cliente */}
+        <ClientLayout>{children}</ClientLayout> 
       </body>
     </html>
   );
